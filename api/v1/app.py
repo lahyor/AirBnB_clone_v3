6bff1b73130e3a682200ac0e96780f7ddced5443
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-29 21:49:30 +0000

    apis

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 
