bf5a784a20d6e36ec8e4e0ebacafa4d83455f2d1
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-29 14:20:02 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,13 @@ def close_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a
+    JSON-formatted 404 status code response"""
+    return jsonify(error="Not found"), 400
+
+
 if __name__ == "__main__":
     api_host = getenv("HBNB_API_HOST", "0.0.0.0")
     api_port = getenv("HBNB_API_PORT", 5000)
