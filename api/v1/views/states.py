9429e8a23d8aeca86900d2706c1a85445b16d648
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-29 14:32:01 +0000

    api

---
+++ api/v1/views/states.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This script """
+
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+import json
+from flask import request
+from models.state import State
+from flask import abort
+
+
+@app_views.route("/states/", strict_slashes=False)
+def show_states():
+    """Return all states objets"""
+    result = []
+    for state in storage.all("State").items():
+        result.append(state[1].to_dict())
+    return jsonify(result)
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False)
+def show_state(state_id):
+    """Return a specifique State object or raise a 404 error"""
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=['DELETE'],strict_slashes=False)
+def delete_state(state_id):
+    """Return a specifique State object or raise a 404 error"""
+    state = storage.get("State", state_id)
+    if state == None:
+        abort(404)
+    storage.delete(state)
+    return jsonify({}), 201
+
+
+
+@app_views.route("/states", methods=['POST'],strict_slashes=False)
+def create_state():
+    request_body = request.get_json()
+    if request_body is None or "name" not in request_body:
+        abort(404)
+    state = State(name=request_body.get("name"))
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'],strict_slashes=False)
+def update_state(state_id):
+    """Return a specifique State object or raise a 404 error"""
+    state = storage.get("State", state_id)
+    request_body = request.get_json()
+    if state == None:
+        abort(404)
+    if request_body is None or "name" not in request_body:
+         abort(404)
+    for key, value in request_body.items():
+        if key not in ["id", "created_at", "updates_at"]:
+           setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 201
