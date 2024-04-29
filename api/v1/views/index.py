d4ad668708ffb224b3edf1e70483548772a59639
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-29 14:16:51 +0000

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,16 @@ import json
 def status():
     """Returns a json response"""
     return jsonify(status="OK")
+
+
+@app_views.route("/stats", strict_slashes=False)
+def count_objects():
+    """Retrieves the number of each objects by type"""
+    return jsonify(
+            amenities=storage.count("Amenity"),
+            cities=storage.count("City"),
+            places=storage.count("Place"),
+            reviews=storage.count("Review"),
+            states=storage.count("State"),
+            users=storage.count("User"),
+    )
